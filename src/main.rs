3a8d5370605f0c05de326c8570bd70a67c9a5d31
thegergo02 <thegergo02@tutanota.com>
2019-12-26 22:45:27 +0100

    func: properly working multi-threading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::thread;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 
 extern crate pancurses;
 
@@ -38,51 +38,35 @@ impl InfoBar {
 
 struct ComBar {
     win: Window,
-    com: String,
 }
 
 impl ComBar {
     pub fn new(max_y: i32) -> ComBar {
         return ComBar {
             win: newwin(1, 0, max_y - 1, 0),
-            com: String::new(),
         }; 
     }
 
-    pub fn handle_ch(&mut self, input: Option<Input>) -> Option<&str> {
-        match input {
-            Some(Input::KeyBackspace) | Some(Input::Character('\u{7f}')) => self.del_ch(),
-            Some(Input::Character(c)) => self.add_ch(c),
-            _ => {},
-        }
-        
-        match input {
-            Some(Input::KeyEnter) | Some(Input::Character('\x0A')) => return Some(&self.com),
-            _ => None,
-        }
-
-    }
-
     pub fn reset_com(&mut self) {
-        self.com.clear();
         self.win.erase();
     }
 
-    pub fn add_ch(&mut self, ch: char) {
-        self.win.printw(ch.to_string());
-        self.com.push(ch);
+    pub fn add_ch(&mut self, ch: String) {
+        self.win.printw(ch);
     }
 
     pub fn del_ch(&mut self) {
         self.win.mv(self.win.get_cur_y(), self.win.get_cur_x() - 1);
         self.win.delch();
-        self.com.pop();
     }
 }
+unsafe impl Send for ComBar {}
+unsafe impl Sync for ComBar {}
 
-fn quit() {
-    endwin();
-    std::process::exit(0);
+pub enum CommandMsg {
+    AddCh(char),
+    DelCh(),
+    ResetCom(),
 }
 
 fn main() {
@@ -90,41 +74,67 @@ fn main() {
     let max_y = _scr.get_max_y();
     pancurses::noecho();
 
-    let (tx_input, rx_com) = mpsc::channel();
-    let (tx_com, rx_curses) = mpsc::channel();
+    let (tx_input, rx_com) = mpsc::channel::<Input>();
+    let (tx_com, rx_curses) = mpsc::channel::<CommandMsg>();
+    let (tx_curses, rx_input) = mpsc::channel::<bool>();
+
+    let com_bar = Arc::new(Mutex::new(ComBar::new(max_y)));
 
     // NCURSES 
+    let com_curses = Arc::clone(&com_bar);
     thread::spawn(move || {   
         let mut info_bar = InfoBar::new();
         info_bar.refresh_text();
         for rec in rx_curses {
-            info_bar.server = rec;
-            info_bar.refresh_text();
+            let mut com_bar = com_curses.lock().unwrap();
+            match rec {
+                CommandMsg::ResetCom() => com_bar.reset_com(),
+                CommandMsg::AddCh(ch) => com_bar.add_ch(ch.to_string()),
+                CommandMsg::DelCh() => com_bar.del_ch(),
+            }
+            drop(com_bar);
+            tx_curses.send(true).unwrap();
         }
     });
     
-    // COOMAND HELPER
+    // COMMAND HELPER
     thread::spawn(move || {
-        for rec in rx_com {
-            tx_com.send(rec).unwrap();
+        let com = String::new();
+
+        let exec_com = |refer: &str| {
+            refer.to_string().clear();
+            tx_com.send(CommandMsg::ResetCom()).unwrap();
+        };
+        let add_ch = |ch: char, refer: &str| {
+            refer.to_string().push(ch);
+            tx_com.send(CommandMsg::AddCh(ch)).unwrap();
+        };
+        let del_ch = |refer: &str| {
+            refer.to_string().pop();
+            tx_com.send(CommandMsg::DelCh()).unwrap();
+        };
+
+        for input in rx_com {
+            match input {
+                Input::KeyEnter | Input::Character('\x0A') => exec_com(&com),
+                Input::KeyBackspace | Input::Character('\u{7f}') => del_ch(&com),
+                Input::Character(ch) => add_ch(ch, &com),
+                _ => {},
+            }
         }
     });
 
     // INPUT WATCHER
+    let com_input = Arc::clone(&com_bar); 
     let handle = thread::spawn(move || {
-        let mut com_bar = ComBar::new(max_y);
-        let exec_com = |com: &str| {
-                    tx_input.send(String::from(com)).unwrap();
-                    true
-        };
         loop {
-            let com = match com_bar.handle_ch(com_bar.win.getch()) {
-                Some(com) => exec_com(com),
-                None => false,
-            };
-            if com {
-                com_bar.reset_com();
+            let com_bar = com_input.lock().unwrap();
+            match com_bar.win.getch() {
+                Some(inp) => tx_input.send(inp).unwrap(),
+                _ => {},
             }
+            drop(com_bar);
+            rx_input.recv().unwrap();
         }
     });
     handle.join().unwrap();
