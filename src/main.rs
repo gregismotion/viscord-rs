0d9371c733faf504f85d284baf5df587292e3a87
thegergo02 <thegergo02@tutanota.com>
2019-12-29 20:07:23 +0100

    prototype: atleast it compiles :shrug:

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,49 @@
 use std::thread;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc};
+use std::io::{stdin, stdout, Stdout, Write};
 
-extern crate pancurses;
+extern crate termion;
+use termion::event::Key;
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::input::TermRead;
+use termion::{clear, cursor};
 
-use pancurses::{initscr, endwin, newwin, A_REVERSE, Window, Input};
-
-struct InfoBar {
-    win: Window,
-    server: String,
-    channel: String,
-    topic: String,
+struct Command {
+    current_mode: InputMode,
+    com: String,
 }
 
-impl InfoBar {
-    pub fn new() -> InfoBar {
-        let info_bar = InfoBar {
-            win: newwin(1, 0, 0, 0), 
-            server: String::from("Not in server"), 
-            channel: String::from("Not in channel"), 
-            topic: String::from("No topic"),
-        };
-        info_bar.win.bkgd(A_REVERSE);
-        info_bar.refresh_text();
-        return info_bar;
+impl Command {
+    pub fn new() -> Command {
+        Command {
+            current_mode: InputMode::NoMode(),
+            com: String::new(),
+        }
     }
 
-    fn print(&self, text: String) {
-        self.win.clear();
-        self.win.printw(text);
-        self.win.refresh();
-    }
-    
-    fn get_text(&self) -> String {
-        format!("{0} | {1} | {2}", self.server, self.channel, self.topic)
-    }
-
-    fn refresh_text(&self) {
-        self.print(self.get_text());
+    pub fn change_mode(&mut self, new_mode: InputMode) {
+        self.current_mode = new_mode;
+        match self.current_mode {
+            InputMode::ComMode() => {
+                self.com.push(':');
+            },
+            _ => {},
+        }
     }
 
-    pub fn set_server(&mut self, name: String) {
-        self.server = name;
-        self.refresh_text();
+    pub fn add_ch(&mut self, ch: char) {
+        self.com.push(ch);
     }
-
-    pub fn set_channel(&mut self, name: String) {
-        self.channel = name;
-        self.refresh_text();
-    }
-
-    pub fn set_topic(&mut self, name: String) {
-        self.topic = name;
-        self.refresh_text();
-    }
-}
-
-struct ComBar {
-    win: Window,
-}
-
-impl ComBar {
-    pub fn new(max_y: i32) -> ComBar {
-        return ComBar {
-            win: newwin(1, 0, max_y - 1, 0),
-        }; 
-    }
-
-    pub fn reset_com(&mut self) {
-        self.win.erase();
-    }
-
-    pub fn add_ch(&mut self, ch: String) {
-        self.win.printw(ch);
-    }
-
     pub fn del_ch(&mut self) {
-        self.win.mv(self.win.get_cur_y(), self.win.get_cur_x() - 1);
-        self.win.delch();
+        self.com.pop();
+    }
+    pub fn clear(&mut self) {
+        self.com.clear();
     }
 }
-unsafe impl Send for ComBar {}
-unsafe impl Sync for ComBar {}
 
 pub enum CommandMsg {
+    IntoComMode(),
     AddCh(char),
     DelCh(),
     ResetCom(),
@@ -91,90 +52,103 @@ pub enum CommandMsg {
     ChangeTopic(String),
 }
 
-fn quit() {
-    endwin();
-    std::process::exit(0);
+pub enum InputMode {
+    NoMode(),
+    ComMode(),
 }
 
 fn main() {
-    let _scr = initscr();
-    let max_y = _scr.get_max_y();
-    pancurses::noecho();
-
-    let (tx_input, rx_com) = mpsc::channel::<Input>();
-    let (tx_com, rx_curses) = mpsc::channel::<CommandMsg>();
-    let (tx_curses, rx_input) = mpsc::channel::<bool>();
-
-    let com_bar = Arc::new(Mutex::new(ComBar::new(max_y)));
+    let (tx_input, rx_com) = mpsc::channel::<Key>();
+    let (tx_com, rx_ui) = mpsc::channel::<CommandMsg>();
 
     // NCURSES 
-    let com_curses = Arc::clone(&com_bar);
-    thread::spawn(move || {   
-        let mut info_bar = InfoBar::new();
-        info_bar.refresh_text();
-        for rec in rx_curses {
-            let mut com_bar = com_curses.lock().unwrap();
+    thread::spawn(move || {
+        let mut stdout = stdout().into_raw_mode().unwrap();
+        let (x_size, y_size) = match termion::terminal_size() {
+            Ok(size) => size,
+            _ => (0, 0),
+        };
+        
+        let into_com_mode = |stdout: &mut RawTerminal<Stdout>| {
+            write!(stdout, 
+                   "{}{}{}", 
+                   cursor::Show,
+                   cursor::Goto(1, y_size - 1),
+                   clear::CurrentLine);
+            stdout.flush().unwrap();
+        };
+        let reset_com = |stdout: &mut RawTerminal<Stdout>| {
+            write!(stdout, 
+                   "{}{}{}", 
+                   cursor::Hide,
+                   cursor::Goto(1, y_size - 1),
+                   clear::CurrentLine);
+            stdout.flush().unwrap();
+        };
+        let add_ch = |stdout: &mut RawTerminal<Stdout>, ch: char| {
+            write!(stdout, 
+                 "{}",
+                 ch);  
+            stdout.flush().unwrap();
+        };
+        let del_ch = |stdout: &mut RawTerminal<Stdout>| {
+            write!(stdout, 
+                 "{}{}{}",
+                 cursor::Left(1),
+                 clear::AfterCursor,
+                 cursor::Right(1));  
+            stdout.flush().unwrap();
+        };
+
+        for rec in rx_ui {
             match rec {
-                CommandMsg::ResetCom() => com_bar.reset_com(),
-                CommandMsg::AddCh(ch) => com_bar.add_ch(ch.to_string()),
-                CommandMsg::DelCh() => com_bar.del_ch(),
-                CommandMsg::ChangeServer(name) => info_bar.set_server(name),
-                CommandMsg::ChangeChannel(name) => info_bar.set_channel(name),           
-                CommandMsg::ChangeTopic(topic) => info_bar.set_topic(topic),
+                CommandMsg::IntoComMode() => into_com_mode(&mut stdout),
+                CommandMsg::ResetCom() => reset_com(&mut stdout),
+                CommandMsg::AddCh(ch) => add_ch(&mut stdout, ch),
+                CommandMsg::DelCh() => del_ch(&mut stdout),
+                CommandMsg::ChangeServer(name) => {},
+                CommandMsg::ChangeChannel(name) => {},           
+                CommandMsg::ChangeTopic(topic) => {},
                 _ => {},
             }
-            drop(com_bar);
-            tx_curses.send(true).unwrap();
         }
     });
     
     // COMMAND HELPER
-    thread::spawn(move || {
-        let com = String::new();
-        tx_com.send(CommandMsg::ChangeChannel(String::from("FUCK"))).unwrap();
-        let exec_com = |com: &str| {
-            match com {
-                ":q" => quit(),
-                ":s" => tx_com.send(CommandMsg::ChangeServer(String::from("FUCK"))).unwrap(),
-                _ => {},
-            }
-        };
-
-        let handle_com = |refer: &str| {
-            exec_com(refer);
-            refer.to_string().clear();
+    thread::spawn(move || {      
+        let mut com = Command::new();
+        
+        let handle_com = |com: &mut Command| {
             tx_com.send(CommandMsg::ResetCom()).unwrap();
+            com.clear();
         };
-        let add_ch = |ch: char, refer: &str| {
-            refer.to_string().push(ch);
-            tx_com.send(CommandMsg::AddCh(ch)).unwrap();
-        };
-        let del_ch = |refer: &str| {
-            refer.to_string().pop();
+        let del_ch = |com: &mut Command| {
             tx_com.send(CommandMsg::DelCh()).unwrap();
+            com.del_ch();
+        };
+        let add_ch = |com: &mut Command, ch: char| {
+            tx_com.send(CommandMsg::AddCh(ch)).unwrap();
+            com.add_ch(ch);
         };
 
         for input in rx_com {
             match input {
-                Input::KeyEnter | Input::Character('\x0A') => handle_com(&com),
-                Input::KeyBackspace | Input::Character('\u{7f}') => del_ch(&com),
-                Input::Character(ch) => add_ch(ch, &com),
+                Key::Char('\n') => handle_com(&mut com),
+                Key::Backspace => del_ch(&mut com),
+                Key::Char(ch) => add_ch(&mut com, ch),
                 _ => {},
             }
         }
     });
 
     // INPUT WATCHER
-    let com_input = Arc::clone(&com_bar); 
     let handle = thread::spawn(move || {
-        loop {
-            let com_bar = com_input.lock().unwrap();
-            match com_bar.win.getch() {
-                Some(inp) => tx_input.send(inp).unwrap(),
+        let stdin = stdin();
+        for key in stdin.keys() {
+            match key {
+                Ok(key) => tx_input.send(key).unwrap(),
                 _ => {},
             }
-            drop(com_bar);
-            rx_input.recv().unwrap();
         }
     });
     handle.join().unwrap();
